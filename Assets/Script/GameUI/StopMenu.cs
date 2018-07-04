5ec2bf7ea60f2e8a3b37788546dd235551c20984
zhangwen <i.am.zw129@gmail.com>
2018-07-04 12:03:02 +0800

    add banner ad

--- Assets/Script/GameUI/StopMenu.cs
+++ Assets/Script/GameUI/StopMenu.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 using System;
 using ZWGames;
+using GoogleMobileAds.Api;
 
 public class StopMenu : MonoBehaviour {
 
@@ -11,6 +12,8 @@ public class StopMenu : MonoBehaviour {
     public Button restartBtn;
     public Button exitBtn;
 
+    private BannerView bannerView;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -18,6 +21,16 @@ public class StopMenu : MonoBehaviour {
         continueBtn.onClick.AddListener(onContinueBtnClick);
         restartBtn.onClick.AddListener(onRestartBtnClick);
         exitBtn.onClick.AddListener(onExitBtnClick);
+
+        requestBannerAd();
+    }
+
+    private void requestBannerAd()
+    {
+        string adUnitId = "ca-app-pub-3029558759541761/9614450584";  //横幅广告ID
+        bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);  //横幅广告实例化（三个参数为：ID、大小、位置） 
+        AdRequest request = new AdRequest.Builder().Build();
+        bannerView.LoadAd(request);
     }
 
     public void setGameEndDisplay(bool isGameEnd)
